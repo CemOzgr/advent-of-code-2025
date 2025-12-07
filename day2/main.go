140b656bd17ac1b05d82e7a85b47dd8fa7e1287c
Cem Ozgur <usugurusenpai@gmail.com>
2025-12-07 23:27:45 +0300

    Solved Day 2 Part 1

--- day2/main.go
+++ day2/main.go
@@ -6,34 +6,92 @@ import (
 	"strings"
 )
 
-type ranger struct {
-	start int
-	end   int
-}
-
-func Solve() {
+func Solve() int {
 	file, fileReadError := os.ReadFile("day2/input.txt")
 	if fileReadError != nil {
 		panic(fileReadError)
 	}
 
-	ranges := strings.Split(string(file), ",")
+	ranges := strings.Split(strings.ReplaceAll(string(file[3:]), "\r\n", ""), ",")
 
-	rangers := make([]ranger, len(ranges))
-	for i, r := range ranges {
-		values := strings.Split(r, "-")
-		start, err1 := strconv.Atoi(values[0])
+	var sumOfInvalidIds int
+	for _, r := range ranges {
+		limits := strings.Split(r, "-")
+
+		start, err1 := strconv.Atoi(limits[0])
 		if err1 != nil {
 			panic(err1)
 		}
-		end, err2 := strconv.Atoi(values[1])
+
+		end, err2 := strconv.Atoi(limits[1])
 		if err2 != nil {
 			panic(err2)
 		}
 
-		rangers[i] = ranger{
-			start: start,
-			end:   end,
+		for productId := start; productId <= end; productId++ {
+			if isIdInvalid(strconv.Itoa(productId)) {
+				sumOfInvalidIds += productId
+			}
 		}
 	}
+
+	return sumOfInvalidIds
+}
+
+func isIdInvalid(productId string) bool {
+	length := len(productId)
+
+	if length%2 != 0 {
+		return false
+	}
+
+	if length == 2 {
+		return productId[0] == productId[1]
+	}
+
+	right := length / 2
+	for left := 0; left < length/2; left++ {
+		if productId[left] != productId[right] {
+			return false
+		}
+
+		right++
+	}
+
+	return true
+}
+
+func isIdInvalidStringCycling(productId string) bool {
+	doubledId := (productId + productId)[1 : len(productId)*2-1]
+
+}
+
+func kmp(text string, pattern string) {
+	lps := constructLps(pattern)
+}
+
+func constructLps(pattern string) []int {
+	lps := make([]int, len(pattern))
+	length := 0
+
+	for i := 1; i < len(pattern); {
+		if pattern[length] == pattern[i] {
+			length++
+			lps[i] = length
+			i++
+
+			continue
+		}
+
+		if length == 0 {
+			lps[i] = 0
+			i++
+
+			continue
+		}
+
+		length = lps[length-1]
+	}
+
+	return lps
 }
--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 ﻿package main
 
 import (
-	"AdventOfCode/day1"
+	"AdventOfCode/day2"
 	"fmt"
 )
 
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	day1Solution := day1.Solve()
+	day1Solution := day2.Solve()
 
-	fmt.Printf("day1: %d", day1Solution)
+	fmt.Printf("day2: %d", day1Solution)
 }
