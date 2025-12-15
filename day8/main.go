5e749dec0bdf2f57fb12f175b748ca11f36024ff
Cem Ozgur <cemozgur9@hotmail.com>
2025-12-15 17:27:58 +0300

    Solved Day 8 Part 2

--- day8/main.go
+++ day8/main.go
@@ -48,26 +48,17 @@ func Solve(input string) int {
 		return pairs[i].distance < pairs[j].distance
 	})
 
+	var result int
 	unionFind := MakeSet(len(points))
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < len(pairs); i++ {
+		if unionFind.Find(pairs[i].point1) != unionFind.Find(pairs[i].point2) {
+			result = points[pairs[i].point1].x * points[pairs[i].point2].x
+		}
+
 		unionFind.Union(pairs[i].point1, pairs[i].point2)
 	}
 
-	var max1, max2, max3 int
-	for _, x := range unionFind.sizes {
-		if x > max1 {
-			max3 = max2
-			max2 = max1
-			max1 = x
-		} else if x > max2 {
-			max3 = max2
-			max2 = x
-		} else if x > max3 {
-			max3 = x
-		}
-	}
-
-	return max1 * max2 * max3
+	return result
 }
 
 func getDistance(p1 Point, p2 Point) int {
